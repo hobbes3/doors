0b8a49c521dbc0a66f5d8c41de2f802db445c980
hobbes3 <hobbes3@gmail.com>
2012-03-04 06:58:05 -0500

    Added Order.next_step

--- doors/admin.py
+++ doors/admin.py
@@ -2,45 +2,151 @@ from django.contrib import admin
 from mysite.doors.models import *
 
 class GroupAdmin( admin.ModelAdmin ) :
-    list_display = ( 'name', 't_created', 't_modified', )
+    list_display = (
+        'name',
+        't_created',
+        't_modified',
+    )
+    search_fields = ( 'name', )
 
 class ItemAdmin( admin.ModelAdmin ) :
-    list_display = ( 'name', 'comment', 'item_type', 'location', 't_created', 't_modified', )
+    list_display = (
+        'name',
+        'comment',
+        'item_type',
+        'location',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'item_type', )
+    search_fields = ( 'name', 'comment', )
 
 class ItemTypeAdmin( admin.ModelAdmin ) :
-    list_display = ( 'name', 'comment', 't_created', 't_modified', )
+    list_display = (
+        'name',
+        'comment',
+        't_created',
+        't_modified',
+    )
     filter_horizontal = ( 'vendors', )
+    search_fields = ( 'name', 'comment', )
 
 class LocationAdmin( admin.ModelAdmin ) :
-    list_display = ( 'comment', 'room', 'floor', 'building', 'prop', 't_created', 't_modified' )
+    list_display = (
+        'comment',
+        'room',
+        'floor',
+        'building',
+        'prop',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'prop', )
+    search_fields = ( 'comment', 'prop', )
 
 class OrderAdmin( admin.ModelAdmin ) :
-    list_display  = ( 'id', 't_created', 't_modified', 'user_created', 'comment', 'status', )
+    list_display  = (
+        'id',
+        'user_created',
+        'action',
+        'next_step',
+        't_created',
+        't_modified',
+    )
     filter_horizontal = ( 'items', )
-    list_filter = ( 'user_created', 'user_status', 't_created', 't_status' )
+    list_filter = (
+        'user_created',
+        'user_action',
+        't_created',
+        't_action',
+        't_followup_one',
+        't_vendor_appt_one',
+        't_vendor_appt_two',
+        't_work_done',
+        't_followup_two',
+        't_paid',
+    )
+    date_hierarchy = 't_created'
+    search_fields = ( 'user_created', 'user_action', )
+
+class OrderCommentAdmin( admin.ModelAdmin ) :
+    list_display = (
+        'order',
+        'user',
+        'comment',
+        'status',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'order', )
+    date_hierarchy = 't_created'
+    search_fields = ( 'order', 'user', 'comment', )
 
 class LogAdmin( admin.ModelAdmin ) :
-    list_display   = ( 'id', 'log_type', 't_created', 'user', 'message', )
-    search_fields  = ( 'message', )
-    list_filter    = ( 'user', 't_created', )
+    list_display = (
+        'id',
+        'log_type',
+        't_created',
+        'user',
+        'message',
+    )
+    list_filter    = ( 'log_type', 'user', 't_created', )
     date_hierarchy = 't_created'
+    search_fields  = ( 'message', )
 
 class PropertyAdmin( admin.ModelAdmin ) :
-    list_display = ( 'name', 'owner', 'address_line_one', 'address_line_two', 'city', 'state', 'zip_code', 't_created', 't_modified', )
+    list_display = (
+        'name',
+        'owner',
+        'address_line_one',
+        'address_line_two',
+        'city',
+        'state',
+        'zip_code',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'owner', 'city', 'zip_code', 'state', )
+    search_fields = ( 'name', 'owner', 'address_line_one', )
 
 class UserAdmin( admin.ModelAdmin ) :
-    list_display = ( 'email', 'group', 'first_name', 'last_name', 'user_type', 'status', 'location', 't_created', 't_modified', )
+    list_display = (
+        'email',
+        'group',
+        'first_name',
+        'last_name',
+        'user_type',
+        'status',
+        'location',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'group', 'location', )
     search_fields = ( 'email', 'first_name', 'last_name', )
 
 class VendorAdmin( admin.ModelAdmin ) :
-    list_display = ( 'name', 'email', 'phone', 'address_line_one', 'address_line_two', 'city', 'state', 'zip_code', 't_created', 't_modified', )
+    list_display = (
+        'name',
+        'email',
+        'phone',
+        'address_line_one',
+        'address_line_two',
+        'city',
+        'state',
+        'zip_code',
+        't_created',
+        't_modified',
+    )
+    list_filter = ( 'city', 'state', 'zip_code', )
+    search_fields = ( 'name', 'email', 'phone', 'address_line_one', )
 
-admin.site.register( Group   , GroupAdmin    )
-admin.site.register( Item    , ItemAdmin     )
-admin.site.register( ItemType, ItemTypeAdmin )
-admin.site.register( Location, LocationAdmin )
-admin.site.register( Log     , LogAdmin      )
-admin.site.register( Order   , OrderAdmin    )
-admin.site.register( Property, PropertyAdmin )
-admin.site.register( User    , UserAdmin     )
-admin.site.register( Vendor  , VendorAdmin   )
+admin.site.register( Group       , GroupAdmin        )
+admin.site.register( Item        , ItemAdmin         )
+admin.site.register( ItemType    , ItemTypeAdmin     )
+admin.site.register( Location    , LocationAdmin     )
+admin.site.register( Log         , LogAdmin          )
+admin.site.register( Order       , OrderAdmin        )
+admin.site.register( OrderComment, OrderCommentAdmin )
+admin.site.register( Property    , PropertyAdmin     )
+admin.site.register( User        , UserAdmin         )
+admin.site.register( Vendor      , VendorAdmin       )
--- doors/models.py
+++ doors/models.py
@@ -10,19 +10,21 @@ class Group( models.Model ) :
         ( 'w', 'read and write' ),
     )
 
-    name          = models.CharField( max_length = 135 )
-    comment       = models.TextField( blank = True )
-    p_group       = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_user        = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_order_self  = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_order_other = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_item_type   = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_item        = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_property    = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_location    = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    p_vendor      = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
-    t_created     = models.DateTimeField( auto_now_add = True, verbose_name = 'created' )
-    t_modified    = models.DateTimeField( auto_now = True, verbose_name = 'modified' )
+    name            = models.CharField( max_length = 135 )
+    comment         = models.TextField( blank = True )
+    p_group         = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_user          = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_order_self    = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_order_other   = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_comment_self  = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_comment_other = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_item_type     = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_item          = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_property      = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_location      = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    p_vendor        = models.CharField( max_length = 1, choices = P_CHOICES, default = 'n' )
+    t_created       = models.DateTimeField( auto_now_add = True, verbose_name = 'created' )
+    t_modified      = models.DateTimeField( auto_now = True, verbose_name = 'modified' )
 
 class Vendor( models.Model ) :
     def __unicode__( self ) :
@@ -31,7 +33,8 @@ class Vendor( models.Model ) :
     name              = models.CharField( max_length = 135 )
     comment           = models.TextField( blank = True )
     phone             = models.CharField( max_length = 135, blank = True )
-    email             = models.CharField( max_length = 135, blank = True )
+    email             = models.EmailField( blank = True )
+    website           = models.URLField( blank = True )
     address_line_one  = models.CharField( max_length = 135 )
     address_line_two  = models.CharField( max_length = 135, blank = True )
     city              = models.CharField( max_length = 135 )
@@ -54,6 +57,14 @@ class User( models.Model ) :
     def __unicode__( self ) :
         return self.first_name + " " + self.last_name
 
+    TYPE_CHOICES = (
+        ( 't', 'tenant'           ),
+        ( 'o', 'property owner'   ),
+        ( 'v', 'vendor'           ),
+        ( 'm', 'property manager' ),
+        ( 'w', 'web user'         ),
+    )
+
     STATUS_CHOICES = (
         ( 'p', 'pending'  ),
         ( 'a', 'active'   ),
@@ -61,17 +72,10 @@ class User( models.Model ) :
         ( 'b', 'banned'   ),
     )
 
-    TYPE_CHOICES = (
-        ( 't', 'tenant'           ),
-        ( 'o', 'property owner'   ),
-        ( 'v', 'vendor'           ),
-        ( 'm', 'property manager' ),
-    )
-
+    email      = models.EmailField()
     group      = models.ForeignKey( Group )
     user_type  = models.CharField( max_length = 1, choices = TYPE_CHOICES, default = 't' )
     comment    = models.TextField( blank = True )
-    email      = models.CharField( max_length = 135 )
     password   = models.CharField( max_length = 135 )
     first_name = models.CharField( max_length = 135 )
     last_name  = models.CharField( max_length = 135 )
@@ -129,22 +133,62 @@ class Order( models.Model ) :
     def __unicode__( self ) :
         return str( self.id )
 
-    STATUS_CHOICES = (
+    def next_step( self ) :
+        user = self.user_created.first_name
+
+        if self.t_action is None :
+            return "1: Review, then either approve or reject the order."
+        elif self.t_followup is None :
+            return "2: Follow up with " + user + "."
+        elif self.t_vendor_appt_one is None :
+            return "3: Contact the vendor to get a quote and arrange an appointment for " + user + "."
+        elif self.t_vendor_appt_two is None :
+            return "4: Review the quote, (get owner approval), then arrange a second appointment for the repairs."
+        elif self.t_work_done is None :
+            return "5: Confirm that the work is done and pay the vendor."
+        elif self.t_followup_two is None :
+            return "6: Follow up again with " + user + "."
+        elif self.paid is None :
+            return "7: Confirm payment and close the order."
+
+    ACTION_CHOICES = (
         ( 'p', 'pending'  ),
         ( 'a', 'approved' ),
         ( 'r', 'rejected' ),
+        ( 'c', 'closed'   ),
     )
 
-    order_previous = models.ForeignKey( 'self', null = True, blank = True )
-    user_created   = models.ForeignKey( User, related_name = 'user_created' )
-    user_modified  = models.ForeignKey( User, related_name = 'user_modified', null = True, blank = True )
-    items          = models.ManyToManyField( Item, null = True, blank = True )
-    comment        = models.TextField( blank = True )
-    status         = models.CharField( max_length = 1, choices = STATUS_CHOICES, default = 'p' )
-    t_status       = models.DateTimeField( null = True, blank = True )
-    user_status    = models.ForeignKey( User, related_name = 'user_status', null = True, blank = True )
-    t_created      = models.DateTimeField( auto_now_add = True, verbose_name = 'created' )
-    t_modified     = models.DateTimeField( auto_now = True, verbose_name = 'modified' )
+    user_created      = models.ForeignKey( User, related_name = 'user_created' )
+    t_created         = models.DateTimeField( auto_now_add = True, verbose_name = 'created' )
+    t_action          = models.DateTimeField( null = True, blank = True, verbose_name = 'action'             )
+    t_followup_one    = models.DateTimeField( null = True, blank = True, verbose_name = 'first follow-up'    )
+    t_vendor_appt_one = models.DateTimeField( null = True, blank = True, verbose_name = 'first appointment'  )
+    t_vendor_appt_two = models.DateTimeField( null = True, blank = True, verbose_name = 'second appointment' )
+    t_work_done       = models.DateTimeField( null = True, blank = True, verbose_name = 'work done'          )
+    t_followup_two    = models.DateTimeField( null = True, blank = True, verbose_name = 'second follow-up'   )
+    t_paid            = models.DateTimeField( null = True, blank = True, verbose_name = 'paid'               )
+    action            = models.CharField( max_length = 1, choices = ACTION_CHOICES, default = 'p' )
+    user_action       = models.ForeignKey( User, related_name = 'user_status', null = True, blank = True )
+    quote             = models.DecimalField( max_digits = 8, decimal_places = 2, null = True, blank = True )
+    payment           = models.DecimalField( max_digits = 8, decimal_places = 2, null = True, blank = True )
+    items             = models.ManyToManyField( Item, null = True, blank = True )
+    t_modified        = models.DateTimeField( auto_now = True, verbose_name = 'modified' )
+
+class OrderComment( models.Model ) :
+    def __unicode__( self ) :
+        return self.order.id
+
+    STATUS_CHOICES = (
+        ( 'v', 'visible' ),
+        ( 'd', 'deleted' ),
+    )
+
+    order      = models.ForeignKey( Order )
+    user       = models.ForeignKey( User )
+    comment    = models.TextField()
+    status     = models.CharField( max_length = 1, choices = STATUS_CHOICES, default = 'v' )
+    t_created  = models.DateTimeField( auto_now_add = True, verbose_name = 'created' )
+    t_modified = models.DateTimeField( auto_now = True, verbose_name = 'modified' )
 
 class Log( models.Model ) :
     def __unicode__( self ) :
