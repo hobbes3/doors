20b87ea25f51d7bb02458b7a3018a95d5a06bed4
hobbes3 <hobbes3@gmail.com>
2012-03-11 12:41:04 -0400

    Using RequestContext, user information will now available on every template.

--- doors/views.py
+++ doors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 from doors.models import *
 
 def index( request ) :
@@ -8,19 +9,24 @@ def order_detail( request, order_id ) :
     order    = get_object_or_404( Order, pk = order_id )
     comments = OrderComment.objects.filter( order = order_id )
 
-    dictionary = {
+    request_context = RequestContext( request, {
         'order'    : order,
         'comments' : comments,
-    }
+    } )
 
-    return render_to_response( 'doors/order/detail.html', dictionary )
+    return render_to_response( 'doors/order/detail.html', context_instance = request_context )
 
 def order_username( request, username ) :
     pass
 
 def order_all( request ) :
     orders = Order.objects.all()
-    return render_to_response( 'doors/order/list.html', { 'orders' : orders } )
+
+    request_context = RequestContext( request, {
+        'orders' : orders,
+    } )
+
+    return render_to_response( 'doors/order/list.html', context_instance = request_context )
 
 def order_create( request ) :
     pass
