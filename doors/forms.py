ad877297c10f13a98ab027e328cc48903434fc0f
hobbes3 <hobbes3@gmail.com>
2012-04-22 04:38:18 -0400

    Applied the initial CSS template from Tareq's link on that SimpleAdmin tutorial. I'm going to work on my own custom comments framework now. I'll be leveraging as much as I can from Django's built-in one first.

--- doors/forms.py
+++ doors/forms.py
@@ -1,15 +1,16 @@
-from django.forms import ModelForm, ModelChoiceField, Select
+from django import forms
 from django.db.models.query import EmptyQuerySet
 from doors.models import Order, Place
 
-class UserModelChoiceField(ModelChoiceField):
+class UserModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return obj.get_full_name()
 
-class OrderCreateForm(ModelForm):
-    class Meta:
-        model=Order
-        fields=('work_type', 'comment',)
+class OrderCreateForm(forms.Form):
+    work_type = forms.ChoiceField(
+        choices=Order.WORK_TYPE_CHOICES
+    )
+    comment = forms.CharField(widget=forms.Textarea)
 
     def __init__(self, creator_list=None, place_list=None, *args, **kwargs):
         super(OrderCreateForm, self).__init__(*args, **kwargs)
@@ -17,19 +18,27 @@ class OrderCreateForm(ModelForm):
         if creator_list:
             self.fields['creator'] = UserModelChoiceField(
                 queryset=creator_list,
-                empty_label="Select a user",
-                widget=Select(attrs={
-                    'onchange': "Dajaxice.doors.orders_create_creator_changed(fill_other_fields, {'creator_pk': this.options[this.selectedIndex].value})"
-                })
+                empty_label="Select a user"
             )
 
         if place_list:
-            self.fields['place'] = ModelChoiceField(
+            self.fields['place'] = forms.ModelChoiceField(
                 queryset=place_list,
                 empty_label=None
             )
         else:
-            self.fields['place'] = ModelChoiceField(
+            self.fields['place'] = forms.ModelChoiceField(
                 queryset=EmptyQuerySet(),
                 empty_label="Select a creator first"
             )
+
+    def clean(self):
+        super(OrderCreateForm, self).clean()
+
+        if 'comment' in self.cleaned_data:
+            if len(self.cleaned_data['comment']) < 50:
+                self._errors['comment'] = self.error_class([u"Please enter a longer comment."])
+
+                del self.cleaned_data['comment']
+
+        return self.cleaned_data
