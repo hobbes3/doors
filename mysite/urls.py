fcf8a3a01fb1921ddcc3f4e9884a23022fc8b79c
hobbes3 <hobbes3@gmail.com>
2012-05-09 06:19:19 -0400

    Fixed mispelling on permanent on RedirectView

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,10 +9,10 @@ admin.autodiscover()
 dajaxice_autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('order_list'), permenant=False), name='home'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('order_list'), permanent=False), name='home'),
     url(r'^doors/', include('doors.urls')),
-    url(r'^accounts/$', RedirectView.as_view(url=reverse_lazy('order_list'), permenant=False), name='account_list'),
-    url(r'^accounts/self/$', RedirectView.as_view(url=reverse_lazy('order_list'), permenant=False), name='account_self_detail'),
+    url(r'^accounts/$', RedirectView.as_view(url=reverse_lazy('order_list'), permanent=False), name='account_list'),
+    url(r'^accounts/self/$', RedirectView.as_view(url=reverse_lazy('order_list'), permanent=False), name='account_self_detail'),
     url(r'^accounts/', include('registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^{}/'.format(settings.DAJAXICE_MEDIA_PREFIX), include('dajaxice.urls')),
